ce584294df9bb9b8e15ad6359d200ad8b7488f78
jvscJoao <joaov1500@gmail.com>
2023-11-13 19:54:27 -0300

    Desafio Calculadora

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.cod3r.exerciciossb.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.exerciciossb.models.Cliente;
@@ -21,4 +22,8 @@ public class ClienteController {
 		return new Cliente(id, "Pedro", "123.456.789-00");
 	}
 
+	@GetMapping
+	public Cliente obterClientePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "Ronaldo", "123.456.789-00");
+	}
 }
