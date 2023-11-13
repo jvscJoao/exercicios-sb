6ad3cf81b20795aabf59192d0c80515ca797fac5
jvscJoao <joaov1500@gmail.com>
2023-11-13 19:31:33 -0300

    Passando parametros pela URL #1

--- src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
@@ -1,13 +1,19 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrimeiroController {
 
-	@GetMapping
+	@GetMapping(path = "/ola")
 	public String ola() {
+		return "Olá Spring Boot!";
+	}
+	
+	@PostMapping(path = "/suda")
+	public String ola2() {
 		return "Olá Spring Boot!";
 	}
 }
